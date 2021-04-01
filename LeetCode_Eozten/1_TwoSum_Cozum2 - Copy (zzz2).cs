b54017e5e8d912c08ee570105fb1c0a43c91b59c
Eren ÖZTEN <priimalshade@gmail.com>
2021-04-01 12:50:31 +0300

    y

---
+++ LeetCode_Eozten/1_TwoSum_Cozum2 - Copy (zzz2).cs
@@ -0,0 +1,32 @@
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+
+//namespace LeetCode_Eozten
+//{
+//    public class Solution
+//    {
+//        static void Main()
+//        {
+//            int[] sampleArray = { 19, 90, 10, 3, 8, 10 };
+//            int target = 100;
+
+//            Solution program = new Solution();
+//            int[] result = program.TwoSum(sampleArray, target);
+
+//        }
+
+//        public int[] TwoSum(int[] nums, int target)
+//        {
+//            Dictionary<int, int> dictionary = new Dictionary<int, int>();
+            
+//            for (int i = 0; i < nums.Length; i++)
+//            {
+//                int diff;
+//                diff = target - nums[i];
+//            }
+
+//            return new[] {1};
+//        }
+//    }
+//}
---
+++ LeetCode_Eozten/1_TwoSum_Cozum2.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode_Eozten
+{
+    public class Solution
+    {
+        static void Main()
+        {
+            int[] sampleArray = { 98, 5, 7, 92, 77, 66 };
+            int target = 12;
+
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            Solution program = new Solution();
+            int[] result = program.TwoSum(sampleArray, target);
+
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+        }
+
+        public int[] TwoSum(int[] nums, int target)
+        {
+            int[] result = new int[] { -1, -1 };
+            if (nums == null)
+                return result;
+            if (nums.Length < 2)
+                return result;
+
+            Dictionary<int, int> map = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                //  -86      12     -   98
+                //  7        12     -   5
+                //  5        12     -   7
+                int diff = target - nums[i];
+                if (map.ContainsKey(diff))
+                {
+                    result[0] = map[diff];
+                    result[1] = i;
+                    return result;
+                }
+
+                //if duplicate number is present [it cannot be the solution]
+                if (map.ContainsKey(nums[i]))
+                    continue;
+                map.Add(nums[i], i);
+            }
+
+            return result;
+        }
+    }
+}
