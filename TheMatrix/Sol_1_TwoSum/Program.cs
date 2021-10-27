9ee00237f721d0b40d3db3cee05f6cb0390a977a
Eren ÖZTEN <priimalshade@gmail.com>
2021-10-27 23:38:48 +0300

    Deleted TheMatrix project. Created Sol_1_TwoSum project

---
+++ TheMatrix/Sol_1_TwoSum/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sol_1_TwoSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
