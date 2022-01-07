8d3366e79dfb336793bd4bf1d2a6a864a8813090
Eren ÖZTEN <priimalshade@gmail.com>
2022-01-08 00:03:25 +0300

    Update Program.cs

--- Quest_9_ PalindromeNumber/Program.cs
+++ Quest_9_ PalindromeNumber/Program.cs
@@ -29,24 +29,70 @@ Explanation: Reads 01 from right to left.Therefore it is not a palindrome.
 */
 
 
-sayı pozitif olmalı. negatif olursa sayının başında "-" olacağı için palindrom olmaz.,
+//sayı pozitif olmalı. negatif olursa sayının başında "-" olacağı için palindrom olmaz.,
 
-public class Solution
-{
-    public static void Main()
-    {
-        Console.WriteLine("Quest_9_ PalindromeNumber \n");
-        Solution solution = new Solution();
+//public class Solution
+//{
+//    public static void Main()
+//    {
+//        Console.WriteLine("Quest_9_ PalindromeNumber \n");
+//        Solution solution = new Solution();
 
-        int number = 12321;
-        var result = solution.IsPalindrome(number);
-    }
+//        int number = 12321;
+//        var result = solution.IsPalindrome(number);
+//    }
 
-    public bool IsPalindrome(int number)
-    {
+//    public bool IsPalindrome(int number)
+//    {
 
-        return false;
-    }
-}
+//        return false;
+//    }
+//}
+
+
+
+//public class Solution
+//{
+//    static void Main()
+//    {
+//        string numberAsText = "1024";
+
+//        Solution solution = new Solution();
+//        var result = solution.IsPalindrome(numberAsText);
+//    }
+
+//    public bool IsPalindrome(string numberAsText)
+//    {
+//        int islemYapilmisHali, remainder, sum = 0, islemYapilmamisHali;
+//        //clrscr();   
+
+//        islemYapilmisHali = Convert.ToInt32(numberAsText);
+
+//        //islemYapilmamisHali değişkenine, gelen sayıyı olduğu gibi yazdık. Çünkü işlemler tamamlandığında, islemYapilmamisHali ile islemYapilmisHali YİNE birbirine eşit olmalı ki polindrome olayı doğru olmuş olsun. Birbirine eşit değilse: polindrome değildir sonucu çıkar.
+//        islemYapilmamisHali = islemYapilmisHali;
+
+//        while (islemYapilmisHali > 0)
+//        {
+//            remainder = islemYapilmisHali % 10; //for getting remainder by dividing with 10    
+//            islemYapilmisHali = islemYapilmisHali / 10; //for getting quotient by dividing with 10    
+//            sum = sum * 10 + remainder;
+//            /*multiplying the sum with 10 and adding  
+//            remainder*/
+//        }
+//        Console.WriteLine("\nThe Reversed Number is: {0} \n", sum);
+
+//        if (islemYapilmamisHali == sum) //checking whether the reversed number is equal to entered number    
+//        {
+//            Console.WriteLine("True!");
+//            return true;
+//        }
+//        else
+//        {
+//            Console.WriteLine("False!");
+//            return false;
+//        }
+//    }
+//}
+
 
 
