326112c4dd6502f91b2506d873f5bf51921391f9
Eren ÖZTEN <priimalshade@gmail.com>
2022-01-09 14:35:37 +0300

    push allllll

---
+++ Leetcode_Questions/Quest_14_Longest_Common_Prefix/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+
+public class Solution
+{
+    public static void Main()
+    {
+        string[] array = new string[] { "fligg6", "flowerrrr11", "flo" };
+
+        Solution solution = new Solution();
+        var result = solution.LongestCommonPrefix(array);
+    }
+
+    public string LongestCommonPrefix(String[] strs)
+    {
+        var result = strs[0];
+
+        for (int i = 1; i < strs.Length; i++)
+        {
+            while (strs[i].StartsWith(result) == false)
+            {
+                result = result.Substring(0, result.Length - 1);
+            }
+        }
+        return result;
+    }
+}
---
+++ Leetcode_Questions/Quest_20_Valid_Parentheses/Program.cs
@@ -0,0 +1,52 @@
+﻿
+public class Solution
+{
+    public static void Main()
+    {
+        //var input = "";
+        var input = "()[]{}[";
+        //var input = "({[]})";
+
+        Solution solution = new Solution();
+        var result = solution.IsValid(input);
+    }
+
+    public bool IsValid(string input)
+    {
+        // Get ready initial state (enforce element type)
+        var stack = new Stack<char>();
+
+        // Evaluate each character for potential mismatch 
+        foreach (char c in input)
+        {
+            // Push closing round bracket onto the stack
+            if (c == '(')
+            {
+                stack.Push(')');
+                continue;
+            }
+            // Push closing curly bracket onto the stack
+            if (c == '{')
+            {
+                stack.Push('}');
+                continue;
+            }
+
+            // Push closing square bracket onto the stack
+            if (c == '[')
+            {
+                stack.Push(']');
+                continue;
+            }
+
+            // Look out for imbalanced strings or mismatches
+            if (c != stack.Pop())
+            {
+                return false;
+            }
+        }
+
+        // Empty stack means string is valid and invalid otherwise
+        return stack.Count == 0;
+    }
+}
