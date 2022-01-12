272b788dbe59744e59eb9f10f9ac4330242e66a9
Eren ÖZTEN <priimalshade@gmail.com>
2022-01-13 01:30:08 +0300

    Remove Element

---
+++ Leetcode_Questions/Quest_27_Remove_Element/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Solution
+{
+    public int RemoveElement(int[] nums, int val)
+    {
+        int increase = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != val)
+            {
+                nums[increase] = nums[i];
+                increase++;
+            }
+        }
+        return increase;
+    }
+}
+
